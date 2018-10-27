022f4b5be62db9f9952fb9030358fc19522e4e74
BritishPandaChick <sbartley1080@gmail.com>
2018-10-27 19:35:00 -0400

    Update themes and plugins

--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -1,10 +1,15 @@
 <?php
 // datastore=auditqueue;
 // created_on=1496372125;
-// updated_on=1496372125;
+// updated_on=1540683076;
 exit(0);
 ?>
 1518572110_4422:"Warning: BritishPandaChick, 127.0.0.1; Themes updated: (multiple entries): Make (v1.8.8; make),Twenty Fifteen (v1.7; twentyfifteen),Twenty Seventeen (v1.2; twentyseventeen),Twenty Sixteen (v1.3; twentysixteen)"
 1518572135_2664:"Critical: BritishPandaChick, 127.0.0.1; WordPress updated to version: 4.9.4"
 1518572313_652:"Error: 127.0.0.1; User authentication failed: BritishPandaChick"
 1518572320_1558:"Error: 127.0.0.1; User authentication failed: BritishPandaChick"
+1540683083_8591:"Notice: 127.0.0.1; User authentication succeeded: BritishPandaChick"
+1540683086_5272:"Notice: BritishPandaChick, 127.0.0.1; Post status has been changed; details: ID: 53,Old status: new,New status: auto-draft,Title: Auto Draft"
+1540683095_1721:"Critical: BritishPandaChick, 127.0.0.1; WordPress updated to version: 4.9.8"
+1540683117_1054:"Warning: BritishPandaChick, 127.0.0.1; Plugins updated: (multiple entries): Akismet Anti-Spam (v4.0.2; akismet\/akismet.php),Disable Comments (v1.7.1; disable-comments\/disable-comments.php),Sucuri Security - Auditing, Malware Scanner and Hardening (v1.8.11; sucuri-scanner\/sucuri.php)"
+1540683130_1902:"Warning: BritishPandaChick, 127.0.0.1; Themes updated: (multiple entries): Make (v1.9.8; make),Twenty Fifteen (v1.9; twentyfifteen),Twenty Seventeen (v1.4; twentyseventeen),Twenty Sixteen (v1.4; twentysixteen)"
---
+++ wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1540683056;
+// updated_on=1540683056;
+exit(0);
+?>
