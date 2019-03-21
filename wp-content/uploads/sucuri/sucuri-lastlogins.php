3c9089967d43841b1c158c2d835c7b164a9c9900
BritishPandaChick <sbartley1080@gmail.com>
2019-03-21 19:36:45 -0400

    Update theme and plugins for WP 5.1.1

--- wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -7,3 +7,4 @@
 {"user_id":1,"user_login":"BritishPandaChick","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-LEVBKH4K","user_lastlogin":"2018-10-27 23:31:23"}
 {"user_id":1,"user_login":"BritishPandaChick","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-LEVBKH4K","user_lastlogin":"2019-01-04 23:04:24"}
 {"user_id":1,"user_login":"BritishPandaChick","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-LEVBKH4K","user_lastlogin":"2019-01-16 02:22:26"}
+{"user_id":1,"user_login":"BritishPandaChick","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-LEVBKH4K","user_lastlogin":"2019-03-21 23:33:25"}
